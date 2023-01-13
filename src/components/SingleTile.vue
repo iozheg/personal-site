a3eb528c596fe3352a21442b02596880e74fcacc
Aleksandr Netsev <aknetsev@gmail.com>
2023-01-13 21:16:17 +0400

    refactor: rename tile component

--- src/components/SingleTile.vue
+++ src/components/SingleTile.vue
@@ -17,23 +17,23 @@ function select() {
 
 <template>
   <div
-    class="tile"
+    class="single-tile"
     :class="{
-      'tile--big': (size || 0) > 1,
-      'tile--clickable': title,
-      'tile--transparent': hide,
-      'tile--hidden': !title
+      'single-tile--big': (size || 0) > 1,
+      'single-tile--clickable': title,
+      'single-tile--transparent': hide,
+      'single-tile--hidden': !title
     }"
     @click="select"
   >
-    <div class="tile__title">
+    <div class="single-tile__title">
       {{ title }}
     </div>
   </div>
 </template>
 
 <style scoped>
-.tile {
+.single-tile {
   width: 256px;
   height: 256px;
   background-color: #FFFFFF;
@@ -41,65 +41,65 @@ function select() {
   transition: opacity 0.5s linear, box-shadow 0.5s, left 0.5s;
 }
 
-.tile--clickable {
+.single-tile--clickable {
   cursor: pointer;
   z-index: 2;
 }
 
-.tile--big {
+.single-tile--big {
   width: 640px;
 }
 
-.tile--clickable:hover {
+.single-tile--clickable:hover {
   box-shadow: 0px 0px 3px 0px #333333;
   z-index: 99;
 }
 
-.tile--transparent {
+.single-tile--transparent {
   opacity: 0;
 }
 
-.tile__title {
+.single-tile__title {
   margin: 15px 30px;
   font-size: 36px;
   color: #000000;
 }
 
 @media (min-width: 480px) {
-  .tile {
+  .single-tile {
     width: 240px;
     height: 240px;
   }
 
-  .tile__title {
+  .single-tile__title {
     font-size: 28px;
   }
 }
 
 @media (min-width: 768px) {
-  .tile {
+  .single-tile {
     width: 256px;
     height: 256px;
   }
 
-  .tile__title {
+  .single-tile__title {
     font-size: 32px;
   }
 }
 
 @media (min-width: 1024px) {
-  .tile__title {
+  .single-tile__title {
     font-size: 36px;
   }
 }
 
 @media (max-width: 480px) {
-  .tile {
+  .single-tile {
     width: 100%;
     height: 100%;
   }
 
-  .tile__title {
+  .single-tile__title {
     font-size: 28px;
   }
 }
