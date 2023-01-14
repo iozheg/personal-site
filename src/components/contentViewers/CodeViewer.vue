d55d6896165e774fb357d8d09f9254f59d01fe57
Aleksandr Netsev <aknetsev@gmail.com>
2023-01-14 17:10:26 +0400

    feat: add basic code viewer

---
+++ src/components/contentViewers/CodeViewer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  content: string;
+}>();
+
+</script>
+
+<template>
+  <div class="code-viewer">
+    <highlightjs
+        language="typescript"
+        :code="content"
+    />
+  </div>
+</template>
---
+++ src/enums.ts
@@ -0,0 +1,3 @@
+export enum CONTENT_TYPES {
+  code = "code"
+}
