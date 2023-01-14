41696088bfb38524a093a060ce238161f0faf6b1
Aleksandr Netsev <aknetsev@gmail.com>
2023-01-14 17:13:42 +0400

    fix: add highlightjs plugin

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import 'highlight.js/styles/vs2015.css';
+import hljs from 'highlight.js/lib/core';
+import typescript from 'highlight.js/lib/languages/typescript';
+import hljsVuePlugin from "@highlightjs/vue-plugin";
+
+
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(router);
+
+hljs.registerLanguage('typescript', typescript);
+app.use(hljsVuePlugin);
 
 app.mount("#app");
