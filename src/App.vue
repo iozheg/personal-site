67304886e891a0dce15978b9078451de9ecf11f3
Aleksandr Netsev <aknetsev@gmail.com>
2023-01-16 01:12:48 +0400

    fix: fix header style

--- src/App.vue
+++ src/App.vue
@@ -14,30 +14,14 @@ import MainMenu from "./components/MainMenu.vue";
 
 <style scoped>
 header {
+  display: flex;
+  flex-direction: column;
   line-height: 1.5;
   max-height: 100vh;
-}
-
-.wrapper {
-  position: relative;
 }
 
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
