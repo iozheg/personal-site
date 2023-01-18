dd3d5d0cbffeed65548ffd029e59a19adf973064
Aleksandr Netsev <aknetsev@gmail.com>
2023-01-19 00:22:45 +0400

    feat: add image viewer

---
+++ src/components/descriptionViewers/ImageViewer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  imageUrl: string;
+}>();
+</script>
+
+<template>
+  <div class="image-viewer">
+    <img class="image-viewer__image" :src="imageUrl" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.image-viewer {
+  align-self: center;
+  min-width: 0;
+  min-height: 0;
+
+  &__image {
+    max-width: 100%;
+    max-height: 100%;
+  }
+}
+</style>
